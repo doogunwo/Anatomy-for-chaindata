19b0c1fa66103851530b4c9717795ca3e90515d9
doogunwo <dgw0601@naver.com>
2024-07-08 18:21:43 +0900

    failed to read block number 오류 발생 tire.go:38

--- sstable/trie.go
+++ sstable/trie.go
@@ -6,12 +6,16 @@ import (
 
     "github.com/ethereum/go-ethereum/node"
     "github.com/ethereum/go-ethereum/core/rawdb"
-    "github.com/ethereum/go-ethereum/common"
 )
 
+const (
+    // 노드 구성 설정
+    cacheSize = 1024
+	handles   = 100
+)
 func main(){
 
-     // 노드 구성 설정
+    //노드 세부 설정
     nodeConfig := node.DefaultConfig
     nodeConfig.DataDir = "/mnt/nvme0n1/ethereum/execution/Fullnode/geth/"
     nodeConfig.Name = "geth"
@@ -23,12 +27,24 @@ func main(){
     }
 
     // Geth 데이터베이스 열기
-    db, err := n.OpenDatabase("chaindata", 0, 0, "", false)
+    db, err := n.OpenDatabase("chaindata", cacheSize, handles, "", false)
     if err != nil {
         log.Fatalf("Failed to open database: %v", err)
     }
     defer db.Close()
     log.Println("Database opened successfully")
-
     
+    // Ancient 데이터베이스 오픈
+    ancient,err := rawdb.NewDatabaseWithFreezer(db, nodeConfig.DataDir+"/chaindata/ancient", "",false)
+    if err != nil{
+        log.Fatalf("failed to open ancient")
+    }
+
+    // 최신 블록 번호 가져오기
+    latestBlock := rawdb.ReadHeaderNumber(ancient, rawdb.ReadHeadHeaderHash(ancient))
+    if latestBlock == nil {
+        log.Fatalf("failed to read block number")
+    }
+
+    fmt.Printf("Latest block number: %d", *latestBlock)
 }
